88eb265ca9007d10fe18d480c12a7dd504c1cd95
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-01 17:56:41 +0300

    Is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,60 +1,60 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_complete - Checks if the binary tree is complete
+ * binary_tree_is_complete -  checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
  *
- * @tree: Pointer to the root node of the tree to check
- *
- * Return: 1 if complete, 0 if NULL or not complete
- */
+ * Return: If tree is NULL, your function must return 0
+*/
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	return (is_complete(tree, 0, binary_tree_size(tree)));
 }
 
 /**
- * is_complete - A helper function to help check for a complete tree
- *
+ * is_complete - check for a complete tree
  * @tree: Pointer to the root node of the tree to check
- * @index: Index of the current node
+ * @level: Index of the current node
  * @size: Number of nodes in the tree
  *
  * Return: 1 if complete, 0 if not or root is NULL
  */
 
 
-int is_complete(const binary_tree_t *tree, int index, int size)
+int is_complete(const binary_tree_t *tree, int level, int size)
 {
 	if (!tree)
 		return (1);
 
-	if (index >= size)
+	if (level >= size)
 		return (0);
 
-	return (is_complete(tree->left, 2 * index + 1, size) &&
-			is_complete(tree->right, 2 * index + 2, size));
+	return (is_complete(tree->left, 2 * level + 1, size) &&
+			is_complete(tree->right, 2 * level + 2, size));
 }
 
 /**
- * binary_tree_size - Measures the size of a binary tree
- *
- * @tree: Pointer to the root node of the tree to measure it's depth
- *
- * Return: If NULL, return 0, otherwise the size of tree
- */
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: If tree is NULL, the function must return 0
+*/
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t left_size;
+	size_t right_size;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
+	}
 
-	count = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
 
-	return (count);
+	return (left_size + right_size + 1);
+	/*+1 is addition for the current node*/
 }
