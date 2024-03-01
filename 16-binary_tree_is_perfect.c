3a032ef942a00a54d1707b187784627809723a1a
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-01 11:37:30 +0300

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,9 +15,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	return ((height_left == height_right) &&
-	binary_tree_is_full(tree->left) &&
-	binary_tree_is_full(tree->left));
+	if (height_left == height_right)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+
+		return(binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+	}
+	return (0);
 }
 
 /**
